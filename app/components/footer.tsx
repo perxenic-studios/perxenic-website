a7ea9305d3d6f2d252c2a23452a28ab57ba32f5f
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2025-09-21 15:27:44 +0100

    Fixed lucky's links

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -46,7 +46,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="luckymcdev.github.io"
+            href="https://luckymcdev.github.io"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">Lucky's Links</p>
